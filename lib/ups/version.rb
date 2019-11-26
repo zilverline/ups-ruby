357563d911d342e6ae1002c86be15224af2f39bf
Luke Farmer <3025530+lefarmer@users.noreply.github.com>
2019-11-26 17:19:22 +0000

    Version 0.13.0

--- lib/ups/version.rb
+++ lib/ups/version.rb
@@ -1,8 +1,8 @@
 module UPS
   module Version
     MAJOR = 0
-    MINOR = 12
-    PATCH = 8
+    MINOR = 13
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
