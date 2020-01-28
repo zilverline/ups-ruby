ca8bc1f42f6d9a4a7db66d4b838396a61627f84a
Luke Farmer <3025530+lefarmer@users.noreply.github.com>
2020-01-28 18:17:28 +0000

    Update XML schema

--- spec/support/xsd_validator.rb
+++ spec/support/xsd_validator.rb
@@ -2,7 +2,7 @@ require 'minitest/assertions'
 
 module Minitest::Assertions
   def assert_passes_validation(schema_path, xml_to_validate)
-    schema = Nokogiri::XML::Schema(File.read(schema_path))
+    schema = Nokogiri::XML::Schema(File.open(schema_path))
     document = Nokogiri::XML::Document.parse(xml_to_validate)
     schema.validate(document)
   end
