753db3b77fb5242cc47b4bbeb2b334e04c3e2d22
Kyle Chapman <kyleichapman@gmail.com>
2024-04-18 18:17:46 +0200

    Update list of packaging and services types

--- lib/ups/packaging.rb
+++ lib/ups/packaging.rb
@@ -1,27 +1,16 @@
 module UPS
   PACKAGING = {
+    '00' => 'UNKNOWN',
     '01' => 'UPS Letter',
-    '02' => 'Customer Supplied Package',
+    '02' => 'Package',
     '03' => 'Tube',
     '04' => 'Pak',
-    '21' => 'UPS Express Box',
-    '24' => 'UPS 25kg box',
-    '25' => 'UPS 10kg box',
+    '21' => 'Express Box',
+    '24' => '25KG Box',
+    '25' => '10KG Box',
     '30' => 'Pallet',
     '2a' => 'Small Express Box',
     '2b' => 'Medium Express Box',
-    '2c' => 'Large Express Box',
-    '56' => 'Flats',
-    '57' => 'Parcels',
-    '58' => 'BPM',
-    '59' => 'First Class',
-    '60' => 'Priority',
-    '61' => 'Machinables',
-    '62' => 'Irregulars',
-    '63' => 'Parcel Post',
-    '64' => 'BPM Parcel',
-    '65' => 'Media Mail',
-    '66' => 'BPM Flat',
-    '67' => 'Standard Flat'
+    '2c' => 'Large Express Box'
   }
 end
